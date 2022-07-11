1ae848052f029426632448531137ea60f58c43c2
sarolta2022 <saroltamail@protonmail.com>
2022-07-11 12:28:22 +0200

    Updates

---
+++ precache-manifest.ed7cf88f409512560852dc05005f0a63.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "8be31ae9af691ed6580c",
+    "url": "/resume/static/css/main.f60a6244.chunk.css"
+  },
+  {
+    "revision": "8be31ae9af691ed6580c",
+    "url": "/resume/static/js/main.8be31ae9.chunk.js"
+  },
+  {
+    "revision": "c6b5a3e0f92ee25b3986",
+    "url": "/resume/static/js/1.c6b5a3e0.chunk.js"
+  },
+  {
+    "revision": "d31443b48e55db6a340f",
+    "url": "/resume/static/js/2.d31443b4.chunk.js"
+  },
+  {
+    "revision": "fe660173fc5450ba6e25",
+    "url": "/resume/static/js/runtime~main.fe660173.js"
+  },
+  {
+    "revision": "1533655178b164eb36f0b96a046c55c1",
+    "url": "/resume/static/media/avatar.15336551.png"
+  },
+  {
+    "revision": "d6f21dc1fbd18def3de423a101a252c1",
+    "url": "/resume/static/media/lemo.d6f21dc1.jpg"
+  },
+  {
+    "revision": "152e29e4f698efcea6589fa09ba10a87",
+    "url": "/resume/static/media/boots.152e29e4.jpg"
+  },
+  {
+    "revision": "374dc5315c4eb312907ef2b0ec3f17ba",
+    "url": "/resume/static/media/direction.374dc531.jpg"
+  },
+  {
+    "revision": "b437e63111601744a3fdb0c6c0190301",
+    "url": "/resume/static/media/hello.b437e631.jpg"
+  },
+  {
+    "revision": "7c6780bd712cefec3a3a9d6aa198d293",
+    "url": "/resume/static/media/tools.7c6780bd.jpg"
+  },
+  {
+    "revision": "8d923a596f76a755aa6b9246dd375251",
+    "url": "/resume/index.html"
+  }
+];
